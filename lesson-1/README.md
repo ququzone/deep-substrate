a62e8de7df11a36b8777e7f3e7104412f3d6f0d7
ququzone <xueping.yang@gmail.com>
2021-01-07 20:23:01 +0800

    complete basic poe module

---
+++ lesson-1/README.md
@@ -0,0 +1,11 @@
+Lesson 1
+========
+
+## node
+
+```
+cd substrate-node-poe
+make init
+make build
+./target/release/node-template --dev --tmp
+```
---
+++ lesson-1/substrate-node-poe/pallets/poe/src/lib.rs
@@ -0,0 +1,89 @@
+#![cfg_attr(not(feature = "std"), no_std)]
+
+use frame_support::{decl_module, decl_storage, decl_event, decl_error, ensure, StorageMap};
+use frame_system::ensure_signed;
+use sp_std::vec::Vec;
+
+#[cfg(test)]
+mod mock;
+
+#[cfg(test)]
+mod tests;
+
+pub trait Trait: frame_system::Trait {
+	type Event: From<Event<Self>> + Into<<Self as frame_system::Trait>::Event>;
+}
+
+decl_storage! {
+	trait Store for Module<T: Trait> as PoeModule {
+		Proofs: map hasher(blake2_128_concat) Vec<u8> => (T::AccountId, T::BlockNumber);
+	}
+}
+
+decl_event!(
+	pub enum Event<T> where AccountId = <T as frame_system::Trait>::AccountId {
+        ClaimCreated(AccountId, Vec<u8>),
+        ClaimRevoked(AccountId, Vec<u8>),
+	}
+);
+
+decl_error! {
+	pub enum Error for Module<T: Trait> {
+		ProofAlreadyClaimed,
+        NoSuchProof,
+        NotProofOwner,
+	}
+}
+
+decl_module! {
+	pub struct Module<T: Trait> for enum Call where origin: T::Origin {
+		// Errors must be initialized if they are used by the pallet.
+		type Error = Error<T>;
+
+		// Events must be initialized if they are used by the pallet.
+		fn deposit_event() = default;
+
+		#[weight = 10_000]
+        fn create_claim(origin, proof: Vec<u8>) {
+            // Check that the extrinsic was signed and get the signer.
+            // This function will return an error if the extrinsic is not signed.
+            // https://substrate.dev/docs/en/knowledgebase/runtime/origin
+            let sender = ensure_signed(origin)?;
+
+            // Verify that the specified proof has not already been claimed.
+            ensure!(!Proofs::<T>::contains_key(&proof), Error::<T>::ProofAlreadyClaimed);
+
+            // Get the block number from the FRAME System module.
+            let current_block = <frame_system::Module<T>>::block_number();
+
+            // Store the proof with the sender and block number.
+            Proofs::<T>::insert(&proof, (&sender, current_block));
+
+            // Emit an event that the claim was created.
+            Self::deposit_event(RawEvent::ClaimCreated(sender, proof));
+        }
+
+        #[weight = 10_000]
+        fn revoke_claim(origin, proof: Vec<u8>) {
+            // Check that the extrinsic was signed and get the signer.
+            // This function will return an error if the extrinsic is not signed.
+            // https://substrate.dev/docs/en/knowledgebase/runtime/origin
+            let sender = ensure_signed(origin)?;
+
+            // Verify that the specified proof has been claimed.
+            ensure!(Proofs::<T>::contains_key(&proof), Error::<T>::NoSuchProof);
+
+            // Get owner of the claim.
+            let (owner, _) = Proofs::<T>::get(&proof);
+
+            // Verify that sender of the current call is the claim owner.
+            ensure!(sender == owner, Error::<T>::NotProofOwner);
+
+            // Remove claim from storage.
+            Proofs::<T>::remove(&proof);
+
+            // Emit an event that the claim was erased.
+            Self::deposit_event(RawEvent::ClaimRevoked(sender, proof));
+        }
+	}
+}
