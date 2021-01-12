e5c9b5095584441e0cf67af99fdbe03678c4f0f9
ququzone <xueping.yang@gmail.com>
2021-01-12 08:36:06 +0800

    complete create action

---
+++ lesson-2/substrate-node-kitties/pallets/kitties/src/lib.rs
@@ -0,0 +1,90 @@
+#![cfg_attr(not(feature = "std"), no_std)]
+
+use codec::{Encode, Decode};
+use frame_support::{decl_module, decl_storage, decl_event, decl_error, ensure, StorageMap, dispatch,
+    traits::{Get, Randomness},
+};
+use sp_io::hashing::blake2_128;
+use frame_system::ensure_signed;
+use sp_runtime::DispatchError;
+
+type KittyIndex = u32;
+
+#[derive(Encode, Decode)]
+pub struct Kitty(pub [u8; 16]);
+
+#[cfg(test)]
+mod mock;
+
+#[cfg(test)]
+mod tests;
+
+pub trait Trait: frame_system::Trait {
+    type Event: From<Event<Self>> + Into<<Self as frame_system::Trait>::Event>;
+
+    type Randomness: Randomness<Self::Hash>;
+}
+
+decl_storage! {
+    trait Store for Module<T: Trait> as Kitties {
+        pub Kitties get(fn kitties): map hasher(blake2_128_concat) KittyIndex => Option<Kitty>;
+        pub KittiesCount get(fn kitties_count): KittyIndex;
+        pub KittyOwners get(fn kitty_owner): map hasher(blake2_128_concat) KittyIndex => Option<T::AccountId>;
+    }
+}
+
+decl_event!(
+	pub enum Event<T> where AccountId = <T as frame_system::Trait>::AccountId {
+        Created(AccountId, KittyIndex),
+    }
+);
+
+decl_error! {
+	pub enum Error for Module<T: Trait> {
+        KittiesCountOverflow,
+	}
+}
+
+decl_module! {
+	pub struct Module<T: Trait> for enum Call where origin: T::Origin {
+        type Error = Error<T>;
+
+		#[weight = 0]
+		pub fn create(origin) {
+            let sender = ensure_signed(origin)?;
+            let kitty_id = Self::next_kitty_id()?;
+            let dna = Self::random_value(&sender);
+
+            let kitty = Kitty(dna);
+			Self::insert_kitty(&sender, kitty_id, kitty);
+
+            Self::deposit_event(RawEvent::Created(sender, kitty_id));
+        }
+    }
+}
+
+impl<T: Trait> Module<T> {
+    fn next_kitty_id() -> sp_std::result::Result<u32, DispatchError> {
+		let kitty_id = Self::kitties_count();
+		if kitty_id == u32::max_value() {
+			return Err(Error::<T>::KittiesCountOverflow.into());
+		}
+		Ok(kitty_id)
+    }
+    
+    fn random_value(sender: &T::AccountId) -> [u8; 16] {
+		let payload = (
+            T::Randomness::random_seed(),
+            &sender,
+            <frame_system::Module<T>>::extrinsic_index(),
+        );
+        payload.using_encoded(blake2_128)
+    }
+    
+    fn insert_kitty(owner: T::AccountId, kitty_id: u32, kitty: Kitty) {
+        Kitties::insert(kitty_id, kitty);
+        KittiesCount::put(kitty_id + 1);
+        <KittyOwners<T>>::insert(kitty_id, owner);
+	}
+}
+
---
+++ lesson-2/substrate-node-kitties/pallets/poe/src/tests.rs
@@ -0,0 +1,108 @@
+use crate::{Error, mock::*};
+use frame_support::{assert_ok, assert_noop};
+use super::*;
+
+#[test]
+fn create_claim_works() {
+    new_test_ext().execute_with(|| {
+        let proof = vec![0, 1];
+
+        assert_ok!(PoeModule::create_claim(Origin::signed(1), proof.clone()));
+        assert_eq!(Proofs::<Test>::get(&proof), (1, frame_system::Module::<Test>::block_number()));
+    })
+}
+
+#[test]
+fn create_claim_fail_when_already_exist() {
+    new_test_ext().execute_with(|| {
+        let proof = vec![0, 1];
+        let _ = PoeModule::create_claim(Origin::signed(1), proof.clone());
+
+        assert_noop!(
+			PoeModule::create_claim(Origin::signed(1), proof.clone()),
+			Error::<Test>::ProofAlreadyClaimed
+		);
+    })
+}
+
+#[test]
+fn revoke_claim_works() {
+    new_test_ext().execute_with(|| {
+        let proof = vec![0, 1];
+        let _ = PoeModule::create_claim(Origin::signed(1), proof.clone());
+
+        assert_ok!(PoeModule::revoke_claim(Origin::signed(1), proof.clone()));
+    })
+}
+
+#[test]
+fn revoke_claim_not_exist() {
+    new_test_ext().execute_with(|| {
+        let proof = vec![0, 1];
+
+        assert_noop!(
+			PoeModule::revoke_claim(Origin::signed(1), proof.clone()),
+			Error::<Test>::NoSuchProof
+		);
+    })
+}
+
+#[test]
+fn revoke_claim_not_owner() {
+    new_test_ext().execute_with(|| {
+        let proof = vec![0, 1];
+        let _ = PoeModule::create_claim(Origin::signed(1), proof.clone());
+
+        assert_noop!(
+			PoeModule::revoke_claim(Origin::signed(2), proof.clone()),
+			Error::<Test>::NotProofOwner
+		);
+    })
+}
+
+#[test]
+fn transfer_claim_works() {
+    new_test_ext().execute_with(|| {
+        let proof = vec![0, 1];
+        let _ = PoeModule::create_claim(Origin::signed(1), proof.clone());
+
+        assert_ok!(PoeModule::transfer_claim(Origin::signed(1), proof.clone(), 2));
+    })
+}
+
+#[test]
+fn transfer_claim_not_exist() {
+    new_test_ext().execute_with(|| {
+        let proof = vec![0, 1];
+
+        assert_noop!(
+			PoeModule::transfer_claim(Origin::signed(1), proof.clone(), 2),
+			Error::<Test>::NoSuchProof
+		);
+    })
+}
+
+#[test]
+fn transfer_claim_not_owner() {
+    new_test_ext().execute_with(|| {
+        let proof = vec![0, 1];
+        let _ = PoeModule::create_claim(Origin::signed(1), proof.clone());
+
+        assert_noop!(
+			PoeModule::transfer_claim(Origin::signed(2), proof.clone(), 3),
+			Error::<Test>::NotProofOwner
+		);
+    })
+}
+
+#[test]
+fn create_claim_fail_when_to_large_hash() {
+    new_test_ext().execute_with(|| {
+        let proof = vec![0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+        assert_noop!(
+			PoeModule::create_claim(Origin::signed(1), proof.clone()),
+			Error::<Test>::ProofTooLarge
+		);
+    })
+}
